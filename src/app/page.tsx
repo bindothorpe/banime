d0db4e96c5339a79aa4af2312945f471996e9493
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-12-04 13:15:51 +0100

    feat: :sparkles: Add HomeResponse type definition and update Home component to use typed API response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { HomeResponse } from "@/types/home-response";
+
 export default async function Home() {
-  let data = await fetch(process.env.API_BASE_URL + "/home");
-  let results = await data.json();
+  const data = await fetch(process.env.API_BASE_URL + "/home");
+  const results: HomeResponse = await data.json();
   console.log(results);
 
   return <div>Home</div>;
