a960d255b56e6c06bae45aeca5e40365fc31b758
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-12-04 22:39:25 +0100

    feat: :sparkles: Remove react-show-more-text dependency and implement custom ShowMoreText component

--- src/components/show-more-text.tsx
+++ src/components/show-more-text.tsx
@@ -1,7 +1,37 @@
 "use client";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
-import ReactShowMoreText from "react-show-more-text";
+interface ShowMoreTextProps {
+  text: string;
+  maxLines?: number;
+  readMoreText?: string;
+  readLessText?: string;
+}
 
-export default function ShowMoreText({ text }: { text: string }) {
-  return <ReactShowMoreText>{text}</ReactShowMoreText>;
+export default function ShowMoreText({
+  text,
+  maxLines = 3,
+  readMoreText = "Show more",
+  readLessText = "Show less",
+}: ShowMoreTextProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div className="relative">
+      <div
+        className={`${!isExpanded ? `line-clamp-${maxLines}` : ""} text-base`}
+      >
+        {text}
+      </div>
+
+      <Button
+        onClick={() => setIsExpanded(!isExpanded)}
+        variant="link"
+        className="p-0"
+      >
+        {isExpanded ? readLessText : readMoreText}
+      </Button>
+    </div>
+  );
 }
