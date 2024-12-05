b66c3a62bf81b02b3ee2cb3b83d71e146dd6ed51
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-12-05 17:36:05 +0100

    feat: :sparkles: Add CustomDialogProps interface and update command component to use it for improved type safety

--- src/components/custom-ui/command.tsx
+++ src/components/custom-ui/command.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import * as React from "react";
-import { type DialogProps } from "@radix-ui/react-dialog";
 import { Command as CommandPrimitive } from "cmdk";
 import { Search } from "lucide-react";
+import { type CustomDialogProps as DialogProps } from "@/types/shadcn/dialog-props";
 
 import { cn } from "@/lib/utils";
 import { Dialog, DialogContent } from "@/components/custom-ui/dialog";
